0ec197d50c269b739f1fbd225720cae6e7472fda
Ashwanth K <97865565+AK16092003@users.noreply.github.com>
2022-08-14 11:24:18 +0530

    Update bubblesort.py
    eliminate swap function

--- bubblesort.py
+++ bubblesort.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python2
 
-def swap(array, i, j):
-    tmp = array[i]
-    array[i] = array[j]
-    array[j] = tmp
-
 def bubblesort(array):
     n = len(array)-1
     for i in range(0, len(array)):
         for j in range(0, n):
             if array[j] > array[j+1]:
-                swap(array, j+1, j)
+                array[j+1], array[j] = array[j], array[j+1] 
         n -= 1
 
 if __name__ == "__main__":
